1dd9c34c66d30802dcf6de76bd84d1702384b1e3
eosoyi <eosoyi@outlook.com>
2024-06-13 08:27:01 -0600

    Se finaliza con la maqueta inicial de la app.

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,10 +1,22 @@
 import { JournalLayout } from "../layout/JournalLayout"
 import { NothingSelectedView } from "../../views/NothingSelectedView"
+import { NoteView } from "../../views/NoteView"
+import { IconButton } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
+      {/* <NoteView></NoteView> */}
       <NothingSelectedView></NothingSelectedView>
+      <IconButton
+        size="large"
+        sx={{
+          color: 'white', backgroundColor: 'error.main', ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed', right: 50, bottom: 50
+        }}>
+        <AddOutlined sx={{ fontSize: 30 }}></AddOutlined>
+      </IconButton>
     </JournalLayout>
   )
 }
---
+++ src/views/NoteView.tsx
@@ -0,0 +1,41 @@
+import { SaveOutlined } from "@mui/icons-material"
+import { Button, Grid, TextField, Typography } from "@mui/material"
+import { ImageGallery } from "../journal/components/ImageGallery"
+
+export const NoteView = () => {
+    return (
+        <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{ mb: 1 }}>
+            <Grid item>
+                <Typography fontSize={39} fontWeight='light'>28 de agosto, 2024</Typography>
+            </Grid>
+            <Grid item>
+                <Button color="primary" sx={{ padding: 2 }}>
+                    <SaveOutlined sx={{ fontSize: 30, mr: 1 }}></SaveOutlined>
+                    Guardar
+                </Button>
+            </Grid>
+            <Grid container>
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    placeholder="Ingrese un título"
+                    label="Título"
+                    sx={{ border: 'none', mb: 1 }}>
+                </TextField>
+                <TextField
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    multiline
+                    placeholder="¿Qué sucedió el dia de hoy?"
+                    minRows={5}
+                    sx={{ border: 'none', mb: 1 }}>
+                </TextField>
+            </Grid>
+
+            <ImageGallery></ImageGallery>
+
+        </Grid>
+    )
+}
